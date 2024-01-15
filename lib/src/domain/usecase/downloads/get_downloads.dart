fd242b06275ae3a80cb284efab0316a349ae899f
Rody Davis <rody.davis.jr@gmail.com>
2024-01-15 13:49:15 -0800

    GetHymnDownloads

--- lib/src/domain/usecase/downloads/get_downloads.dart
+++ lib/src/domain/usecase/downloads/get_downloads.dart
@@ -2,22 +2,78 @@ import 'dart:convert';
 
 import 'package:pocketbase/pocketbase.dart';
 
+import '../../../data/source/database/database.dart';
 import '../../../data/source/pocketbase/client.dart';
-import '../../../data/source/pocketbase/get_records.dart';
 import '../../model/hymn_download.dart';
 
-class GetDownloads extends GetRecords<HymnDownload, void> {
-  GetDownloads({required super.db, required super.client})
-      : super(
-          collectionName: 'bundles',
-          itemFromRecordModel: (record) => record.toHymnDownload(client),
-          itemFromRecord: (record) =>
-              RecordModel.fromJson(jsonDecode(record.data))
-                  .toHymnDownload(client),
-          getRecords: (user) => db.getRecordModelsByCollection('bundles'),
-          // fields: 'id,created,updated,hash,file,hymn_id,expand.hymn_id.title,expand.hymn_id.number',
-          expand: 'hymn_id',
-        );
+class GetHymnDownloads {
+  final HfwDatabase db;
+  final HfwStudio client;
+
+  GetHymnDownloads({required this.db, required this.client});
+
+  Stream<List<HymnDownload>> call() async* {
+    final current = await db.getRecordModelsByCollection('bundles').get();
+    final related = current
+        .where((e) => e.deleted != true)
+        .map((e) => RecordModel.fromJson(jsonDecode(e.data)))
+        .map((e) => e.toHymnDownload(client))
+        .toList();
+    yield related;
+    final status = await db //
+        .getCollectionSyncedStatus('bundles')
+        .getSingleOrNull();
+    final now = DateTime.now();
+    const duration = Duration(days: 1);
+    final newest = related.isEmpty
+        ? null
+        : related.length == 1
+            ? related.first.updated
+            : related
+                .reduce((a, b) => a.updated.isAfter(b.updated) ? a : b)
+                .updated;
+    var needsUpdate = status == null || related.isEmpty;
+    if (status != null) {
+      needsUpdate = status.updated.add(duration).isBefore(now);
+    }
+    if (needsUpdate) {
+      try {
+        final remote = await client.collection('bundles').getFullList(
+              filter: newest == null
+                  ? null
+                  : "updated > '${newest.toIso8601String()}'",
+              expand: 'hymn_id',
+            );
+        if (remote.isNotEmpty) {
+          for (final item in remote) {
+            await db.setRecordModel(
+              jsonEncode(item.toJson()),
+              true,
+              false,
+            );
+          }
+          await db.setCollectionSyncedStatus(
+            'bundles',
+            true,
+            now,
+            now,
+          );
+        }
+      } catch (e) {
+        // ignore: avoid_print
+        print('error updating bundles: $e');
+      }
+    }
+
+    final stream = db.getRecordModelsByCollection('bundles').watch();
+    await for (final items in stream) {
+      yield items
+          .where((e) => e.deleted != true)
+          .map((e) => RecordModel.fromJson(jsonDecode(e.data)))
+          .map((e) => e.toHymnDownload(client))
+          .toList();
+    }
+  }
 }
 
 extension HymnDownloadRecordModel on RecordModel {
