53f9cb5cd2d9bfceaee9d955c87c9a5f189596c4
Rody Davis <rody.davis.jr@gmail.com>
2024-01-15 01:25:44 -0800

    fix path

--- lib/src/domain/usecase/library/hymns/get_music_for_hymn.dart
+++ lib/src/domain/usecase/library/hymns/get_music_for_hymn.dart
@@ -1,6 +1,7 @@
 import '../../../model/bundle_file.dart';
 import '../../../model/hymn_archive.dart';
 
+
 class GetMusicForHymn {
   List<BundleFile> call(HymnArchive archive) {
     return archive.getMusic();
@@ -9,10 +10,7 @@ class GetMusicForHymn {
 
 extension GetMusicForHymnX on HymnArchive {
   List<BundleFile> getMusic() {
-    final results = findFiles((e) =>
-        e.name.contains('PresentationCreator/assets') &&
-        e.name.contains('- Print Large') &&
-        e.name.endsWith('.png'));
+    final results = findFiles((e) => e.name.contains('PresentationCreator/assets') && e.name.contains('- Print Large') && e.name.endsWith('.png'));
     results.sort((a, b) => a.path.compareTo(b.path));
     return results;
   }
